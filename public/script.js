7a6c1cbd8b20f0bb4727b46c5fae9ac59ba69eae
Dafne Coconi <dafnecoconi@Dafnes-Mac-mini.local>
2025-06-18 05:23:43 -0600

    Display result

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
 document.getElementById("diagnosticForm").addEventListener("submit", async (e) => {
       e.preventDefault();
       const message = document.getElementById("symptoms").value;
-      const response = await fetch("/api/chat", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message })
-      });
-
+      try{
+        const response = await fetch("/api/chat", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message })
+        });
+      console.log("After getting response")
       const data = await response.json();
-      document.getElementById("result").innerText = data.reply;
+      console.log("El data es", data)
+      const resultDiv = document.getElementById('result');
+      //document.getElementById("result").innerText = data.reply;
+      resultDiv.innerHTML = `
+            <h3>Diagnóstico para Perro</h3>
+            <p><strong>Posible enfermedad:</strong> ${data.respuesta}</p>
+        `;
+      console.log("Después")
+      resultDiv.style.display = 'block';
+    } catch (error) {
+        console.log(error)
+        alert("Error al obtener diagnóstico")
+    }
     });
